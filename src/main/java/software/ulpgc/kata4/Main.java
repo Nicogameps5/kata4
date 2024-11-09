65cb360a28ea6e9bfa1f790730efa4e999796fba
Nico <jkjkloljbjk@gmail.com>
2024-11-09 17:19:03 +0000

    Main class

--- src/main/java/software/ulpgc/kata4/Main.java
+++ src/main/java/software/ulpgc/kata4/Main.java
@@ -8,16 +8,8 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        File file = new File("D:/Descargas/title.basics.tsv");
-        List<Title> titles = new FileTitleLoader(file, new TsvTitleDeserializer()).load();
-        Map<Integer, Integer> stats = new HashMap<>();
-        for (Title title : titles) {
-            stats.put(title.year(), stats.getOrDefault(title.year(), 0) + 1);
-        }
-
-        for (int year : stats.keySet()) {
-            System.out.println(year + "\t" + stats.get(year));
-        }
+        ImportCommand command = new ImportCommand();
+        command.execute();
 
     }
 }
